7320d678c1c3727520be74c769f07a95ff1068aa
Nurul Islam <83104019+alwaysraihan@users.noreply.github.com>
2023-02-19 23:14:34 +0600

    intro page done and hoem page basics started

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: const [
+            // good morning
+            Text("Good morning,")
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:grocery_app/pages/home_page.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
@@ -9,22 +11,49 @@ class IntroPage extends StatelessWidget {
       body: Column(children: [
         // logo
         Padding(
-          padding:
-              const EdgeInsets.only(left: 80.0, right: 40, bottom: 80, top: 16),
-          child: Image.asset('lib/images/avocado.png'),
+          padding: const EdgeInsets.only(
+              left: 80.0, right: 80, bottom: 40, top: 100),
+          child: Image.asset(
+            'lib/images/avocado.png',
+            height: 180,
+            width: 180,
+          ),
         ),
         //we deli  ver groceries at your doorstep
-        const Padding(
-          padding: EdgeInsets.all(24.0),
+        Padding(
+          padding: const EdgeInsets.all(24.0),
           child: Text(
             'We deliver your groceries to your doorstep',
             textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 40,
-              fontWeight: FontWeight.normal,
+            style: GoogleFonts.notoSerif(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
             ),
           ),
-        )
+        ),
+
+        // fresh items everyday
+        const Text("Fresh items everyday"),
+        const Spacer(),
+        // get started button
+        GestureDetector(
+          onTap: () => Navigator.pushReplacement(context, MaterialPageRoute(
+            builder: (context) {
+              return const HomePage();
+            },
+          )),
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.deepPurple,
+                borderRadius: BorderRadius.circular(12)),
+            padding: const EdgeInsets.all(24),
+            child: const Text(
+              "Get Started",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        const Spacer(),
       ]),
     );
   }
